fbd1702d8b154aae5c61140fbac83c3cf83b4dd6
Marlon <marlon.noble94@gmail.com>
2021-03-10 20:05:51 +0800

    Add SearchController view model

---
+++ SetScheduleTest/SetScheduleTest/Scenes/Search/EventCellViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  EventCellViewModel.swift
+//  SetScheduleTest
+//
+//  Created by Marlon on 3/10/21.
+//
+
+import Foundation
+
+protocol EventCellViewModel {
+  
+}
--- SetScheduleTest/SetScheduleTest/Scenes/Search/SearchControllerViewModel.swift
+++ SetScheduleTest/SetScheduleTest/Scenes/Search/SearchControllerViewModel.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+protocol SearchControllerViewModel: AnyObject {
+  var isLoadingHandler: ((Bool) -> Void)? { get set }
+  var eventCellViewModels: [EventCellViewModel] { get }
+  var didUpdateEvents: (() -> Void)? { get set }
+}
