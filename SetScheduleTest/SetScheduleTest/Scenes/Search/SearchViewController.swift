d1c5fe05bdc14238c4cd821c846ea0d0b3baee25
Marlon <marlon.noble94@gmail.com>
2021-03-10 20:05:23 +0800

    WIP add search table view

--- SetScheduleTest/SetScheduleTest/Scenes/Search/SearchViewController.swift
+++ SetScheduleTest/SetScheduleTest/Scenes/Search/SearchViewController.swift
@@ -5,4 +5,85 @@
 //  Created by Marlon on 3/10/21.
 //
 
-import Foundation
+import UIKit
+
+class SearchViewController: UITableViewController {
+  
+  private let viewModel: SearchControllerViewModel
+
+  private let searchController = UISearchController(searchResultsController: nil)
+  
+  // TODO: - Call this from UserDefaults
+  private var distance: Double = 10000
+  
+  init(_ viewModel: SearchControllerViewModel) {
+    self.viewModel = viewModel
+    
+    super.init(style: .plain)
+    setup()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+}
+
+// MARK: - Setup
+
+private extension SearchViewController {
+  func setup() {
+    setupSearchController()
+    setupViewModel()
+  }
+  
+  func setupViewModel() {
+    
+  }
+  
+  private func setupSearchController() {
+    navigationItem.rightBarButtonItem = .init(
+      image: UIImage(systemName: "gearshape.2"),
+      style: .done,
+      target: self,
+      action: #selector(showSettingsVC)
+    )
+    navigationItem.searchController = searchController
+    navigationItem.hidesSearchBarWhenScrolling = false
+    searchController.searchBar.delegate = self
+    searchController.obscuresBackgroundDuringPresentation = false
+    searchController.searchBar.placeholder = "Search Here!"
+  }
+}
+
+// MARK: - TableView
+
+extension SearchViewController {
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    viewModel.eventCellViewModels.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    UITableViewCell()
+  }
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    // Handle event URL
+  }
+}
+
+// MARK: - UISearchBarDelegate
+
+extension SearchViewController: UISearchBarDelegate {
+  func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    
+  }
+}
+
+// MARK: - Handlers
+
+private extension SearchViewController {
+  @objc func showSettingsVC() {
+    
+  }
+}
